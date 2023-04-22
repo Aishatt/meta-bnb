569b9677cb485670f38361a54ba30ae7a3da5413
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-04-22 20:09:58 +0100

    Style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
+import logo from '/src/assets/logo.png'
+
 export default function Header() {
     return(
-        <div>
-            <h1>HELLO</h1>
+        <div className='header-container'>
+            <img src={logo} className='logo' alt='meta bnb logo' height="30px" />
+            <ul className='menu-items'>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Place to stay</a>
+                </li>
+                <li>
+                    <a href="">NFTs</a>
+                </li>
+                <li>
+                    <a href="">Community</a>
+                </li>
+            </ul>
+            <button className='wallet-button'>Connect wallet</button>
+            {/*POP-UP!*/}
         </div>
     )
 }
